2c7e914fceef838f9fa5f0c6fec04a4e38c0f8a7
Ninja <jiachenning@gmail.com>
2017-06-12 18:48:33 +0800

    start to train strings

--- hello.py
+++ hello.py
@@ -5,17 +5,23 @@ import sys
 
 # Gather our code in main() function
 def main():
-    print'Hello there', sys.argv[1]
-    # Command line args are in sys.argv[1], sys.argv[2]
-    # sys.argv[0] is the script name itself and can be ignored
+    print repeat('Yay', False) ## YayYayYay
+    print repeat('Woo Hoo', True) ## WOO HooWoo HooWoo Hoo!!!
+
+# Defines a "repeat" function that takes 2 arguments
+def repeat(s, exclaim):
+    """
+    Returns the string 's' repeated 3 times.
+    If exclaim is true, add exclamation marks.
+    """
+
+    result = s + s + s # can also use "s * 3 " which is faster (Why?)
+    if exclaim:
+        result = result + '!!!'
+    return result
+
 
 #Standard boilerplate to call the main() function to begin
 # the program
 if __name__ == '__main__':
-	main()
-
-# def double(num):
-#     """Function to double the value111"""
-#     return 2*num
-
-# print (double.__doc__)
+    main()
