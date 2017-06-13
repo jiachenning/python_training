48130c6cdc323ac8bc9b5bd1f5dad1653ef5987e
Ninja <jiachenning@gmail.com>
2017-06-13 18:58:14 +0800

    train string day2

--- string.py
+++ string.py
@@ -1,3 +1,19 @@
-def main(){
-    
-}
+import sys
+
+def main():
+    s = 'hi'
+    print s[1]
+    print len(s)
+    print s + ' there'
+    pi = 3.14
+    ##text = 'The value of pi is ' + pi ## No, does not work
+    text = 'The value of pi is ' + str(pi) ## yes
+    print text
+    raw = r'this\t\n and that'
+    print raw       ## this\t\n and that
+    multi = """It was the best of times.
+    It was the worst of times."""
+    print multi
+
+if __name__ == '__main__':
+    main()
